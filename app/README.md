d347b65efc3c5848d6039bbf6a9966e7b48abd2e
Rituraj Singh <www.riturajsingh001@gmail.com>
2023-05-24 21:56:36 +0530

    Initial commit

---
+++ app/README.md
@@ -0,0 +1,85 @@
+# Flask Firebase Authentication Application
+
+Welcome to my simple web application built with Flask and Firebase for user authentication. This application allows users to create accounts, sign in to their accounts, manage their profiles, and reset their passwords. It's a great starting point for those interested in implementing Firebase Authentication into their Flask applications.
+
+## Core Features
+
+- **User Registration:** New users can create their own accounts.
+- **User Authentication:** Existing users can authenticate into their accounts.
+- **Session Management:** The application maintains user sessions to keep the users signed in.
+- **Profile Management:** Users can view their profile information.
+- **Password Reset:** Users can reset their forgotten passwords.
+- **Firebase Integration:** The application integrates with Firebase for back-end services.
+- **UI Design:** The application comes with pre-designed user interface for common authentication tasks.
+- **Security:** Passwords are securely managed, and sessions are properly handled to avoid common security issues.
+
+## Prerequisites
+
+Before you start, you need to have the following:
+
+- Python (Version 3.6 or above)
+- Flask
+- Pyrebase (A Python wrapper for the Firebase API)
+
+## Setup Instructions
+
+Follow these steps to get the application running:
+
+1. Clone this repository to your local machine.
+    ```bash
+    git clone https://github.com/rituraj009/Flask-Firebase-Authentication
+    ```
+
+2. Navigate into the project directory.
+    ```bash
+    cd app
+    ```
+
+3. Install the required packages.
+    ```bash
+    pip install -r requirements.txt
+    ```
+
+4. Setup your Firebase project:
+
+    - Navigate to the [Firebase Console](https://console.firebase.google.com/)
+    - Create a new Firebase project.
+    - Under "Your Apps" in "Project Settings", select the "Config" radio button to get your Firebase SDK config.
+    - Replace the "config" variables in `app.py` with your own Firebase project credentials.
+
+    It should look like this:
+
+    ```python
+    config = {
+    "apiKey": "<your-api-key>",
+    "authDomain": "<your-auth-domain>",
+    "databaseURL": "<your-database-url>",
+    "storageBucket": "<your-storage-bucket>"
+    }
+    ```
+    
+    - Enable the Firebase Realtime Database by clicking on Realtime Database in the left panel, and adjust the rules setting to enable read and write access.
+
+    ```json
+    {
+      "rules": { ".read": true, ".write": true }
+    }
+    ```
+
+## Running the Application
+
+To start the application, execute the following command in your terminal:
+
+```bash
+python app.py
+```
+
+## Screenshots
+![login](https://github.com/rituraj009/Flask-Firebase-Authentication/assets/102078863/145b7652-558a-46e4-87de-c82205d6cd52)
+
+Welcome Page for now
+![profile](https://github.com/rituraj009/Flask-Firebase-Authentication/assets/102078863/8ebf25be-02b5-4055-9a89-cee8b3d63043)
+
+## Contributing
+Contributions are very welcome! Feel free to fork the repository and submit pull requests. If you want to make major changes, please open an issue first to discuss what you'd like to change. If you encounter any issues or have any questions, don't hesitate to open an issue. We appreciate any feedback!
+
---
+++ app/app.py
@@ -0,0 +1,144 @@
+# Import necessary modules
+import pyrebase
+from flask import Flask, flash, redirect, render_template, request, session, abort, url_for
+from datetime import datetime
+import re
+
+# Create a new Flask application
+app = Flask(__name__)
+# Set the secret key for the Flask app. This is used for session security.
+app.secret_key = "your_secret_key"
+
+# Configuration for Firebase
+config = {
+    "apiKey": "",
+    "authDomain": "",
+    "databaseURL": "",
+    "storageBucket": ""
+}
+
+# Initialize Firebase
+firebase = pyrebase.initialize_app(config)
+
+# Get reference to the auth service and database service
+auth = firebase.auth()
+db = firebase.database()
+
+# Route for the login page
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+# Route for the signup page
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+# Route for the welcome page
+@app.route("/welcome")
+def welcome():
+    # Check if user is logged in
+    if session.get("is_logged_in", False):
+        return render_template("welcome.html", email=session["email"], name=session["name"])
+    else:
+        # If user is not logged in, redirect to login page
+        return redirect(url_for('login'))
+
+# Function to check password strength
+def check_password_strength(password):
+    # At least one lower case letter, one upper case letter, one digit, one special character, and at least 8 characters long
+    return re.match(r'^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$', password) is not None
+
+# Route for login result
+@app.route("/result", methods=["POST", "GET"])
+def result():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        try:
+            # Authenticate user
+            user = auth.sign_in_with_email_and_password(email, password)
+            session["is_logged_in"] = True
+            session["email"] = user["email"]
+            session["uid"] = user["localId"]
+            # Fetch user data
+            data = db.child("users").get().val()
+            # Update session data
+            if data and session["uid"] in data:
+                session["name"] = data[session["uid"]]["name"]
+                # Update last login time
+                db.child("users").child(session["uid"]).update({"last_logged_in": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")})
+            else:
+                session["name"] = "User"
+            # Redirect to welcome page
+            return redirect(url_for('welcome'))
+        except Exception as e:
+            print("Error occurred: ", e)
+            return redirect(url_for('login'))
+    else:
+        # If user is logged in, redirect to welcome page
+        if session.get("is_logged_in", False):
+            return redirect(url_for('welcome'))
+        else:
+            return redirect(url_for('login'))
+
+# Route for user registration
+@app.route("/register", methods=["POST", "GET"])
+def register():
+    if request.method == "POST":
+        result = request.form
+        email = result["email"]
+        password = result["pass"]
+        name = result["name"]
+        if not check_password_strength(password):
+            print("Password does not meet strength requirements")
+            return redirect(url_for('signup'))
+        try:
+            # Create user account
+            auth.create_user_with_email_and_password(email, password)
+            # Authenticate user
+            user = auth.sign_in_with_email_and_password(email, password)
+            session["is_logged_in"] = True
+            session["email"] = user["email"]
+            session["uid"] = user["localId"]
+            session["name"] = name
+            # Save user data
+            data = {"name": name, "email": email, "last_logged_in": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")}
+            db.child("users").child(session["uid"]).set(data)
+            return redirect(url_for('welcome'))
+        except Exception as e:
+            print("Error occurred during registration: ", e)
+            return redirect(url_for('signup'))
+    else:
+        # If user is logged in, redirect to welcome page
+        if session.get("is_logged_in", False):
+            return redirect(url_for('welcome'))
+        else:
+            return redirect(url_for('signup'))
+
+# Route for password reset
+@app.route("/reset_password", methods=["GET", "POST"])
+def reset_password():
+    if request.method == "POST":
+        email = request.form["email"]
+        try:
+            # Send password reset email
+            auth.send_password_reset_email(email)
+            return render_template("reset_password_done.html")  # Show a page telling user to check their email
+        except Exception as e:
+            print("Error occurred: ", e)
+            return render_template("reset_password.html", error="An error occurred. Please try again.")  # Show error on reset password page
+    else:
+        return render_template("reset_password.html")  # Show the password reset page
+
+# Route for logout
+@app.route("/logout")
+def logout():
+    # Update last logout time
+    db.child("users").child(session["uid"]).update({"last_logged_out": datetime.now().strftime("%m/%d/%Y, %H:%M:%S")})
+    session["is_logged_in"] = False
+    return redirect(url_for('login'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
